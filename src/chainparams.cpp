a33825fa660813173599e65aa41e9344ecc395e0
tanyang <tanyang@yy.com>
2018-11-13 16:00:35 +0800

    add two checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -287,6 +287,14 @@ public:
                 // Nov, 13 DAA activation block.
                 {504031, uint256S("0x0000000000000000011ebf65b60d0a3de80b8175be"
                                   "709d653b4c1a1beeb6ab9c")},
+								  
+                // Hard fork for bitcoin candy
+                {512666，uint256S("0x00043bfba38c60e8b283d4e507e5785b8cd5f72b8b"
+                                  "72f66542b2e4c62bd2ed79")},
+				
+                // roll back point of coin inflation attack
+                {671663，uint256S("0x00000241636f7d345b239a2dd785b8e834b494de4d"
+                                  "307535d7afb7b1cb84a641")},
             }};
 
         // Data as of block
